9c16d38e62d85de56a702792e3102f22447555b6
iulianstan <iulian1226@gmail.com>
2021-02-08 15:28:19 +0100

    Password generator

---
+++ extraWork.js
@@ -0,0 +1,97 @@
+// Extra work :)
+// create a repo for this app. we are coming later to it :)
+// Create a program that generate a random password with the length of 30 character,
+// every time you call the function should give you a different password
+
+// Examples you will have other passwords for sure
+
+// passwordGen()  ➞ Y3mI9x7bvPY86T?oM§hIbgh!MLwA77N
+// passwordGen()  ➞ Oy$tkBGoo5fg§mO4AdV&Kh8XXZR4d7K
+// passwordGen()  ➞ v0ZwDrn@hfI€kOKWH§k6kqub6zfpuyP
+
+const passwordGen = () => {
+  let characters = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    6,
+    7,
+    8,
+    9,
+    "!",
+    "§",
+    "$",
+    "%",
+    "&",
+    "?",
+    "@",
+    "€",
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  let result = "";
+  let randomNum;
+  for (let i = 0; i <= 30; i++) {
+    randomNum = Math.floor(Math.random() * characters.length);
+    // result = result + characters[randomNum];
+    result += characters[randomNum];
+  }
+  return result;
+};
+
+console.log(passwordGen());
+console.log(passwordGen());
+console.log(passwordGen());
+console.log(passwordGen());
